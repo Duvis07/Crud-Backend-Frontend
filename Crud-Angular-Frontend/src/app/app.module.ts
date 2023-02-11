5b92082ab06486796fbbfe8b99674f57a42ef591
Duvis07 <duvan.botero@est.iudigital.edu.co>
2023-02-11 11:37:07 -0500

    se crea base del proyecto

--- Crud-Angular-Frontend/src/app/app.module.ts
+++ Crud-Angular-Frontend/src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//External Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { ProductoComponent } from './models/producto/producto.component';
+import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
