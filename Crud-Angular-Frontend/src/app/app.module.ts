abe283512483c09f9689af5a7f88735586e12605
Duvis07 <duvan.botero@est.iudigital.edu.co>
2023-02-11 11:52:40 -0500

    se reorganiza el proyecto

--- Crud-Angular-Frontend/src/app/app.module.ts
+++ Crud-Angular-Frontend/src/app/app.module.ts
@@ -3,32 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './componentes/modelo/producto/producto.component';
+import { NuevoProductoComponent } from './componentes/producto/nuevo-producto/nuevo-producto.component';
+import { ListaProductoComponent } from './componentes/producto/lista-producto/lista-producto.component';
+import { DetalleProductoComponent } from './componentes/producto/detalle-producto/detalle-producto.component';
+import { EditarProductoComponent } from './componentes/producto/editar-producto/editar-producto.component';
 
 //External Modules
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ToastrModule } from 'ngx-toastr';
-import { ProductoComponent } from './models/producto/producto.component';
-import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
-import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
-import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
-import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+import { ToastrModule } from 'ngx-toastr'; */
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductoComponent,
+    NuevoProductoComponent,
     ListaProductoComponent,
     DetalleProductoComponent,
-    NuevoProductoComponent,
     EditarProductoComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
+    /* BrowserAnimationsModule,
+    ToastrModule.forRoot(), */
   ],
   providers: [],
   bootstrap: [AppComponent]
