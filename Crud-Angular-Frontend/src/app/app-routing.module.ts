cd158e8aa7c06b3a8538dcf6fac7f1674e00f3e9
Duvis07 <duvan.botero@est.iudigital.edu.co>
2023-02-12 19:59:18 -0500

    se listan los productos

--- Crud-Angular-Frontend/src/app/app-routing.module.ts
+++ Crud-Angular-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleProductoComponent } from './componentes/producto/detalle-producto/detalle-producto.component';
+import { EditarProductoComponent } from './componentes/producto/editar-producto/editar-producto.component';
+import { ListaProductoComponent } from './componentes/producto/lista-producto/lista-producto.component';
+import { NuevoProductoComponent } from './componentes/producto/nuevo-producto/nuevo-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListaProductoComponent},
+  {path: 'detalle/:id', component: DetalleProductoComponent},
+  {path: 'nuevo', component: NuevoProductoComponent},
+  {path: 'editar/:id', component: EditarProductoComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Crud-Angular-Frontend/src/app/componentes/producto/lista-producto/lista-producto.component.ts
+++ Crud-Angular-Frontend/src/app/componentes/producto/lista-producto/lista-producto.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/service/producto.service';
+import { Producto } from '../../modelo/producto';
 
 @Component({
   selector: 'app-lista-producto',
   templateUrl: './lista-producto.component.html',
-  styleUrls: ['./lista-producto.component.css']
+  styleUrls: ['./lista-producto.component.css'],
 })
 export class ListaProductoComponent implements OnInit {
+  productos: Producto[] = [];
+  toastr: any;
 
-  constructor() { }
+  constructor(private productoService: ProductoService) {}
 
   ngOnInit(): void {
+    this.cargarProductos();
   }
 
+  cargarProductos(): void {
+    this.productoService.lista().subscribe(
+      (data) => {
+        this.productos = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  borrar(id: number) {
+    this.productoService.delete(id).subscribe(
+      (data) => {
+        this.toastr.success('Producto Eliminado', 'OK', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        this.cargarProductos();
+      },
+      (err) => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+      }
+    );
+  }
 }
